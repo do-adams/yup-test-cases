a37f62d58be92a42654f7498851d077e32f4b7db
Damian <contact@mianlabs.com>
2019-12-11 10:25:23 -0600

    Add index.js script file

---
+++ index.js
@@ -0,0 +1,23 @@
+const yup = require('yup');
+
+function run(title, ctx, fn, ...arguments) {
+    const result = fn.call(ctx, ...arguments);
+    console.log(title, result);
+    return result;
+};
+
+function checkStringValidity(title, schema) {
+    run(`${title}: Truthy String`, schema, schema.isValidSync, 'string');
+    run(`${title}: Falsy String`, schema, schema.isValidSync, '');
+    run(`${title}: Null`, schema, schema.isValidSync, null);
+    run(`${title}: Undefined`, schema, schema.isValidSync, undefined);
+}
+
+const defaultSchema = yup.string();
+checkStringValidity('Default Schema', defaultSchema);
+
+const notRequiredSchema = yup.string().notRequired();
+checkStringValidity('Not Required Schema', notRequiredSchema);
+
+const requiredSchema = yup.string().required();
+checkStringValidity('Required Schema', requiredSchema);
